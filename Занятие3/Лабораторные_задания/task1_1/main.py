0943147c6d11694f0601a3950a7ad6c8126de393
maksfom <fett.94@mail.ru>
2022-03-31 21:25:41 +0300

    добавлен файл

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -4,6 +4,18 @@ def main():
     max_sum = 500  # максимальная сумма
 
     # TODO  найти количество чисел сумма квадратов, которых не превышает 500
+    while True:
+        current_value = n ** 2
+
+        if current_sum + current_value >= max_sum:
+            break
+
+
+        current_sum = current_sum + current_value
+        n = n + 1
+
+    return n , current_sum
+
 
 
 if __name__ == "__main__":
--- Занятие3/Лабораторные_задания/task1_2/main.py
+++ Занятие3/Лабораторные_задания/task1_2/main.py
@@ -1,5 +1,13 @@
 # TODO запишите здесь функцию factorial
+def factorial(n):
+    count = 1
+    for i in range(1, n+1):
+        count = count * i
+    return count
+
 
 
 if __name__ == "__main__":
     # TODO распечатать результат выполнения функции factorial от числа 5
+    factorial_input = int(input("введите факториал: "))
+    print(factorial(factorial_input))
